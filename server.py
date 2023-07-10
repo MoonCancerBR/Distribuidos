7510d8308cb368c66e22b2ef2f1aa2bf7e0b3b13
Luís Fernando de Sá Amorim <40233089+MoonCancerBR@users.noreply.github.com>
2023-07-10 12:50:11 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,45 +1,45 @@
 import socket
-import os
+import threading
 
-# Configurações do servidor
-HOST = '127.0.0.1'  # Endereço IP do servidor
-PORT = 12345  # Porta para escutar as conexões
+class ChatServer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.clients = []
+        self.server_socket = None
 
-# Diretório onde os arquivos serão armazenados
-SAVE_DIR = 'arquivos'
+    def start(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(5)
+        print("Chat server started on {}:{}".format(self.host, self.port))
 
-# Cria o diretório de armazenamento, se não existir
-if not os.path.exists(SAVE_DIR):
-    os.makedirs(SAVE_DIR)
-
-# Cria um socket TCP/IP
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Associa o socket ao endereço e porta do servidor
-sock.bind((HOST, PORT))
-
-# Aguarda por conexões
-sock.listen(1)
-print(f"Aguardando conexões na porta {PORT}...")
-
-while True:
-    # Espera por uma conexão
-    conn, addr = sock.accept()
-    print(f"Conexão estabelecida com {addr[0]}:{addr[1]}")
-
-    # Recebe o nome do arquivo
-    filename = conn.recv(1024).decode()
-    filepath = os.path.join(SAVE_DIR, filename)
-
-    # Recebe e armazena o arquivo
-    with open(filepath, 'wb') as file:
         while True:
-            data = conn.recv(1024)
-            if not data:
+            client_socket, client_address = self.server_socket.accept()
+            client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
+            client_thread.start()
+
+    def handle_client(self, client_socket):
+        client_name = client_socket.recv(1024).decode()
+        self.broadcast("{} entrou no chat.".format(client_name))
+
+        self.clients.append((client_name, client_socket))
+
+        while True:
+            try:
+                message = client_socket.recv(1024).decode()
+                if message:
+                    self.broadcast("{}: {}".format(client_name, message))
+            except Exception as e:
+                print("Erro:", str(e))
+                self.clients.remove((client_name, client_socket))
+                self.broadcast("{} saiu do chat.".format(client_name))
                 break
-            file.write(data)
 
-    print(f"Arquivo '{filename}' recebido e armazenado.")
+    def broadcast(self, message):
+        for _, client_socket in self.clients:
+            client_socket.send(message.encode())
 
-    # Fecha a conexão com o cliente
-    conn.close()
+if __name__ == "__main__":
+    server = ChatServer('localhost', 5000)
+    server.start()
