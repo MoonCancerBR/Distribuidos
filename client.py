95119f11f6152110a39566420437e2bb7b293472
Luís Fernando de Sá Amorim <40233089+MoonCancerBR@users.noreply.github.com>
2023-07-10 12:50:43 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,29 +1,36 @@
 import socket
+import threading
 
-# Configurações do cliente
-HOST = '127.0.0.1'  # Endereço IP do servidor
-PORT = 12345  # Porta do servidor
+class ChatClient:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.client_socket = None
 
-# Caminho completo do arquivo que será enviado
-FILE_PATH = '/caminho/completo/do/arquivo.txt'
+    def start(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.host, self.port))
+        print("Conectado ao servidor.")
 
-# Conecta-se ao servidor
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
+        client_name = input("Digite seu nome: ")
+        self.client_socket.send(client_name.encode())
 
-# Envia o nome do arquivo
-filename = FILE_PATH.split('/')[-1]
-sock.sendall(filename.encode())
+        receive_thread = threading.Thread(target=self.receive_messages)
+        receive_thread.start()
 
-# Envia o arquivo
-with open(FILE_PATH, 'rb') as file:
-    while True:
-        data = file.read(1024)
-        if not data:
-            break
-        sock.sendall(data)
+        while True:
+            message = input()
+            self.client_socket.send(message.encode())
 
-print(f"Arquivo '{filename}' enviado com sucesso.")
+    def receive_messages(self):
+        while True:
+            try:
+                message = self.client_socket.recv(1024).decode()
+                print(message)
+            except Exception as e:
+                print("Erro:", str(e))
+                break
 
-# Fecha a conexão com o servidor
-sock.close()
+if __name__ == "__main__":
+    client = ChatClient('localhost', 5000)
+    client.start()
