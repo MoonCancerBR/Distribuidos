78951882adf29fbe47989db9061679aa91c26ff0
Luís Fernando de Sá Amorim <40233089+MoonCancerBR@users.noreply.github.com>
2023-07-11 17:59:49 -0300

    Update client.py

--- client.py
+++ client.py
@@ -1,36 +1,47 @@
 import socket
 import threading
+import json
 
+
+# Classe para o cliente de chat
 class ChatClient:
     def __init__(self, host, port):
-        self.host = host
-        self.port = port
-        self.client_socket = None
-
-    def start(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((self.host, self.port))
-        print("Conectado ao servidor.")
+        self.client_socket.connect((host, port))
 
-        client_name = input("Digite seu nome: ")
-        self.client_socket.send(client_name.encode())
-
-        receive_thread = threading.Thread(target=self.receive_messages)
-        receive_thread.start()
-
+    def send_message(self):
         while True:
             message = input()
-            self.client_socket.send(message.encode())
+            message_data = {"type": "message", "message": message}
+            message_json = json.dumps(message_data)
+            self.client_socket.send(message_json.encode())
 
-    def receive_messages(self):
+    def receive_message(self):
         while True:
-            try:
-                message = self.client_socket.recv(1024).decode()
-                print(message)
-            except Exception as e:
-                print("Erro:", str(e))
-                break
+            message_json = self.client_socket.recv(1024).decode()
+            message_data = json.loads(message_json)
+            message_type = message_data["type"]
+            if message_type == "join":
+                name = message_data["name"]
+                print(f"{name} entrou no chat.")
+            elif message_type == "message":
+                name = message_data["name"]
+                message = message_data["message"]
+                print(f"{name}: {message}")
 
-if __name__ == "__main__":
-    client = ChatClient('localhost', 5000)
-    client.start()
+
+# Configurações do cliente
+HOST = 'localhost'
+PORT = 5000
+
+# Conectando ao servidor
+name = input("Digite seu nome: ")
+client = ChatClient(HOST, PORT)
+client.client_socket.send(name.encode())
+
+# Iniciando as threads para enviar e receber mensagens
+send_thread = threading.Thread(target=client.send_message)
+receive_thread = threading.Thread(target=client.receive_message)
+
+send_thread.start()
+receive_thread.start()
